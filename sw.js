b6b2520db42eb11d47a8903b92b591d478ac0d97
atafghanen <attentacion3@gmail.com>
2026-09-21 11:06:01 +0200

    Refresh PWA cache for search stability

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "at-afghanen-app-v28";
+const CACHE_NAME = "at-afghanen-app-v29";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=77",
-  "./app.js?v=45",
+  "./styles.css?v=78",
+  "./app.js?v=46",
   "./pwa.js?v=11",
   "./manifest.webmanifest",
   "./assets/app-icon.svg",
